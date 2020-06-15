a8d9882309438bc8eb1e5a81a00593b758a38509
Niko Hujanen <niko.hujanen@gmail.com>
2020-06-15 23:16:28 +0300

    Initial commit, alpha version

---
+++ frmMain.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Modplay
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.barProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.lblProgress = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblLink = new System.Windows.Forms.ToolStripStatusLabel();
+            this.trackVolume = new System.Windows.Forms.TrackBar();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.txtAbout = new System.Windows.Forms.RichTextBox();
+            this.lblSong = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(87, 38);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play/Next";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.barProgress,
+            this.lblProgress,
+            this.lblSong,
+            this.lblLink});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 155);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(566, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // barProgress
+            // 
+            this.barProgress.Name = "barProgress";
+            this.barProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.lblProgress.Size = new System.Drawing.Size(10, 17);
+            // 
+            // lblLink
+            // 
+            this.lblLink.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.lblLink.IsLink = true;
+            this.lblLink.Name = "lblLink";
+            this.lblLink.Size = new System.Drawing.Size(147, 17);
+            this.lblLink.Text = "github.com/wex/modplay";
+            this.lblLink.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblLink.Click += new System.EventHandler(this.lblLink_Click);
+            // 
+            // trackVolume
+            // 
+            this.trackVolume.LargeChange = 2;
+            this.trackVolume.Location = new System.Drawing.Point(105, 12);
+            this.trackVolume.Maximum = 40;
+            this.trackVolume.Name = "trackVolume";
+            this.trackVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackVolume.Size = new System.Drawing.Size(45, 133);
+            this.trackVolume.TabIndex = 6;
+            this.trackVolume.TickFrequency = 2;
+            this.trackVolume.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackVolume.Value = 40;
+            this.trackVolume.Scroll += new System.EventHandler(this.trackVolume_Scroll);
+            this.trackVolume.ValueChanged += new System.EventHandler(this.trackVolume_ValueChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 107);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 38);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(67, 72);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(32, 13);
+            this.lblVolume.TabIndex = 8;
+            this.lblVolume.Text = "-0 dB";
+            this.lblVolume.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtAbout
+            // 
+            this.txtAbout.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtAbout.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAbout.Location = new System.Drawing.Point(156, 12);
+            this.txtAbout.Name = "txtAbout";
+            this.txtAbout.ReadOnly = true;
+            this.txtAbout.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.txtAbout.Size = new System.Drawing.Size(398, 133);
+            this.txtAbout.TabIndex = 9;
+            this.txtAbout.Text = "";
+            this.txtAbout.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.txtAbout_LinkClicked);
+            // 
+            // lblSong
+            // 
+            this.lblSong.IsLink = true;
+            this.lblSong.Name = "lblSong";
+            this.lblSong.Size = new System.Drawing.Size(292, 17);
+            this.lblSong.Spring = true;
+            this.lblSong.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblSong.Click += new System.EventHandler(this.lblSong_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 177);
+            this.Controls.Add(this.txtAbout);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.trackVolume);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnPlay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modplay Alpha";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar barProgress;
+        private System.Windows.Forms.ToolStripStatusLabel lblLink;
+        private System.Windows.Forms.TrackBar trackVolume;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolStripStatusLabel lblProgress;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.RichTextBox txtAbout;
+        private System.Windows.Forms.ToolStripStatusLabel lblSong;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using Un4seen.Bass;
+
+namespace Modplay
+{
+    public partial class frmMain : Form
+    {
+        int stream;
+        int song = 1;
+        float _gainAmplification = 1;
+        Random rand;
+        BASSTimer _updateTimer;
+        private DSPPROC myDSPAddr = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtAbout.Rtf = @"{\rtf1\ansi\ansicpg1252\deff0\deflang1033{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
+\viewkind4\uc1\pard\sa200\sl240\slmult1\lang11\b\f0\fs20 Modplay\b0  powered by\par
+\pard\sa200\sl240\slmult1\tx1704 The Mod Archive\tab https://modarchive.org/\line BASS \tab https://www.un4seen.com/\line ZXTUNE \tab https://zxtune.bitbucket.io/\line BASSZXTUNE \tab https://sourceforge.net/projects/basszxtune/\par
+Source available: \tab https://github.com/wex/modplay\par
+}";
+
+            if (!Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_LATENCY, this.Handle))
+            {
+                MessageBox.Show("Initializing audio buffer failed!");
+                Application.Exit();
+            }
+
+            Bass.BASS_PluginLoad("basszxtune.dll");
+
+            SetVolume(trackVolume.Maximum, trackVolume.Maximum);
+
+            rand = new Random();
+
+            _updateTimer = new BASSTimer(100);
+            _updateTimer.Tick += new EventHandler(timerUpdate_Tick);
+
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _updateTimer.Tick -= new EventHandler(timerUpdate_Tick);
+
+            Bass.BASS_Stop();
+            Bass.BASS_Free();
+        }
+
+        private void SetVolume(int v, int max)
+        {
+            lblVolume.Text = $"{v - max} dB";
+            _gainAmplification = (float)Math.Pow(10d, (double) (v - max) / 20d);
+        }
+
+        unsafe private void MyDSPGainUnsafe(int handle, int channel, IntPtr buffer, int length, IntPtr user)
+        {
+            if (_gainAmplification == 1f || length == 0 || buffer == IntPtr.Zero)
+                return;
+
+            int l4 = length / 4;
+
+            float* data = (float*)buffer;
+            for (int a = 0; a < l4; a++)
+            {
+                *data = *data * _gainAmplification;
+                data++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _updateTimer.Stop();
+            Bass.BASS_StreamFree(stream);
+
+            song = rand.Next(1, 189357);
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile($@"https://api.modarchive.org/downloads.php?moduleid={song}", "song.tmp");
+            }
+
+            lblSong.Text = $"Mod Archive #{song}";
+            lblSong.Tag = song;
+
+            stream = Bass.BASS_StreamCreateFile(@"song.tmp", 0, 0, BASSFlag.BASS_SAMPLE_FLOAT | BASSFlag.BASS_STREAM_PRESCAN);
+
+            myDSPAddr = new DSPPROC(MyDSPGainUnsafe);
+            Bass.BASS_ChannelSetDSP(stream, myDSPAddr, IntPtr.Zero, 2);
+
+            Bass.BASS_ChannelPlay(stream, true);
+            _updateTimer.Start();
+        }
+
+        private void timerUpdate_Tick(object sender, System.EventArgs e)
+        {
+            if (Bass.BASS_ChannelIsActive(stream) == BASSActive.BASS_ACTIVE_PLAYING)
+            {
+                
+            }
+            else
+            {
+                _updateTimer.Stop();
+                button1_Click(sender, e);
+                return;
+            }
+
+            long pos = Bass.BASS_ChannelGetPosition(stream);
+            long len = Bass.BASS_ChannelGetLength(stream);
+            double totaltime = Bass.BASS_ChannelBytes2Seconds(stream, len);
+            double elapsedtime = Bass.BASS_ChannelBytes2Seconds(stream, pos);
+
+            lblProgress.Text = String.Format("{0:#0.00} / {1:#0.00}", Utils.FixTimespan(elapsedtime, "MMSS"), Utils.FixTimespan(totaltime, "MMSS"));
+            barProgress.Maximum = (int)totaltime;
+            barProgress.Value = (int)elapsedtime;
+        }
+
+        private void lblLink_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/wex/modplay");
+        }
+
+        private void trackVolume_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackVolume_ValueChanged(object sender, EventArgs e)
+        {
+            SetVolume(trackVolume.Value, trackVolume.Maximum);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtAbout_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void lblSong_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"https://modarchive.org/index.php?request=view_by_moduleid&query={lblSong.Tag}");
+            System.Diagnostics.Process.Start($"https://modarchive.org/index.php?request=view_by_moduleid&query={lblSong.Tag}");
+        }
+    }
+}
